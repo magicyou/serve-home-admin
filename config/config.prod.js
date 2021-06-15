2ac09c4d94386a0588da97d07ed2075c3b554967
lixiaolong <243644435@qq.com>
2021-06-15 16:28:21 +0800

    feat: 完善 配置文件

--- config/config.prod.js
+++ config/config.prod.js
@@ -2,9 +2,9 @@ exports.mysql = {
     client: {
         host: 'localhost',
         port: '3306',
-        user: 'root',
-        password: '111111',
-        database: 'serve-home-admin',
+        user: 'home_admin',
+        password: 'c88de7a6c613d3b2',
+        database: 'home_admin',
     },
     app: true,
     agent: false,
